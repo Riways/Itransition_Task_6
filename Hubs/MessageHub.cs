3d3475aaea15ee30d0885dd67f803151b33a30b6
Viktar Baradzin <a0fragile@gmail.com>
2022-11-29 06:54:06 +0300

    new user notificzation

--- Hubs/MessageHub.cs
+++ Hubs/MessageHub.cs
@@ -6,10 +6,14 @@ namespace Task_6_Blazor_Server.Hubs
     public class MessageHub : Hub
     {
 
-
         public async Task AddMessage(UserModel reciever, MessageModel message)
         {
             await Clients.All.SendAsync("RecieveMessage", reciever, message);
+        }
+        
+        public async Task NewUser(UserModel user)
+        {
+            await Clients.All.SendAsync("AddUser", user);
         }
     }
 }
